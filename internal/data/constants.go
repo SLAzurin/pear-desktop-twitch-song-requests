e2b1aff90616e8223ae0ed164e84db894544f23b
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-12-02 21:52:48 -0500

    remove unused vars

--- internal/data/constants.go
+++ internal/data/constants.go
@@ -1,12 +1,13 @@
 package data
 
 var twitchClientID = "7k7nl6w8e0owouonj7nb9g3k5s6gs5"
-var pearDesktopHost = "localhost:26538"
+var pearDesktopHostname = "localhost"
+var pearDesktopPort = "26538"
 
 func GetTwitchClientID() string {
 	return twitchClientID
 }
 
 func GetPearDesktopHost() string {
-	return pearDesktopHost
+	return pearDesktopHostname + ":" + pearDesktopPort
 }
