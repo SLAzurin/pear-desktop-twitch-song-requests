f012ccb7e34891daecbefd302abe57470ecb7bb7
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-12-09 20:05:47 -0500

    fix: queue push element

--- cmd/main/handle_pear_desktop_msgs.go
+++ cmd/main/handle_pear_desktop_msgs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/azuridayo/pear-desktop-twitch-song-requests/internal/songrequests"
 	"github.com/valyala/fastjson"
 )
 
@@ -39,16 +40,32 @@ func (a *App) handlePearDesktopMsgs() {
 				playerInfoMutex.Unlock()
 			case "VIDEO_CHANGED":
 				playerInfoMutex.Lock()
+				songQueueMutex.Lock()
+				newVideoId := string(v.GetStringBytes("song", "videoId"))
 				playerInfo.Position = v.GetInt("position")
-				songinfo := playerSonginfo{
-					ImageSrc:         string(v.GetStringBytes("song", "imageSrc")),
-					Artist:           string(v.GetStringBytes("song", "artist")),
-					SongDuration:     v.GetInt("song", "songDuration"),
-					AlternativeTitle: string(v.GetStringBytes("song", "alternativeTitle")),
-					VideoId:          string(v.GetStringBytes("song", "videoId")),
+				if playerInfo.Song.VideoId != newVideoId {
+					songinfo := playerSonginfo{
+						ImageSrc:         string(v.GetStringBytes("song", "imageSrc")),
+						Artist:           string(v.GetStringBytes("song", "artist")),
+						SongDuration:     v.GetInt("song", "songDuration"),
+						AlternativeTitle: string(v.GetStringBytes("song", "alternativeTitle")),
+						VideoId:          newVideoId,
+					}
+					playerInfo.Song = songinfo
+					if len(songQueue) > 1 {
+						songQueue = songQueue[1:]
+					}
+					if len(songQueue) > 1 && songQueue[0].song.VideoID != newVideoId {
+						// queue invalid now, wiping queue
+						songQueue = []struct {
+							requestedBy string
+							song        songrequests.SongResult
+						}{}
+						log.Println("queue was wiped because it was out of sync with Pear Desktop")
+					}
 				}
-				playerInfo.Song = songinfo
 				playerInfoMutex.Unlock()
+				songQueueMutex.Unlock()
 			case "PLAYER_STATE_CHANGED":
 				playerInfoMutex.Lock()
 				playerInfo.Position = v.GetInt("position")
